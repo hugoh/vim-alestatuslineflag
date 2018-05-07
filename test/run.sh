50f596a54e1a5077f4d65913778179e958a2b32b
Hugo Haas <hugoh@hugoh.net>
2018-05-06 17:59:14 -0700

    Getting rid of not-a-term

--- test/run.sh
+++ test/run.sh
@@ -3,5 +3,5 @@
 DIR="$(dirname "$0")"
 cd $DIR/..
 
-vim --not-a-term -n -Nu "$DIR/vimrc_test" -c 'Vader! test/*.vader' > /dev/null
+vim -n -Nu "$DIR/vimrc_test" -c 'Vader! test/*.vader' > /dev/null
 
